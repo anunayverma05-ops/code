a267a61f6e19d572fd5fb09cb764c5b4edb76756
Anunay Verma <anunay.verma05@gmail.com>
2026-05-21 14:18:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestUniqueSubstring(self, s: str) -> int:
+        
+        char_map = {}
+        max_len = 0
+        left = 0  
+        
+        
+        for right in range(len(s)):
+            current_char = s[right]
+            
+            
+            if current_char in char_map and char_map[current_char] >= left:
+                left = char_map[current_char] + 1
+            
+            
+            char_map[current_char] = right
+            
+            
+            max_len = max(max_len, right - left + 1)
+            
+        return max_len
